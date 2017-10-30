608261dd02e532bbd74911d558373f719b432ebd
Kornraphop Kawintiranon <kornraphop@Kornraphops-MacBook-Pro.local>
2017-10-30 19:36:45 -0400

    feat: LOF on all cleaned files

--- src/analysis-lof/analysis_lof.py
+++ src/analysis-lof/analysis_lof.py
@@ -4,24 +4,69 @@ import numpy as np
 from sklearn import preprocessing
 from sklearn.neighbors import LocalOutlierFactor
 
-# Global variables
-SCRIPT_DIR = ""
 
-def LOF(data_file_path):
+def LOF(
+		data_file_path,
+		k_list=[5, 20, 50]):
+	"""
+    Use Local Outlier Facter algorithm to find outliers on the specific file
+    :param data_file_path: The specific file path for input data
+    :param k_list: (Optional) The list of neighbor numbers for LOF algorithm, default is k=5,20,50
+    :return: None
+    """
+
 	# Make csv data to dataframe
-	df = pd.read_csv(data_file_path)
+	df = pd.read_csv(data_file_path, encoding='latin-1')
 
 	# Print sample data in dataframe
 	print("========== Sample data ==========")
 	print(df.iloc[:10])
 	print()
 
-	# Get rid of index, area_id: These features should not be used for using LOF because they are unique
+	train_df = df
+
+	# ===== Preprocessing ===== #
+
+	# - Zillow file
+	# 
+	# Remove zpid column (Zillow ID)
+	if 'zpid' in list(train_df):
+		train_df = train_df.drop(['zpid'], axis=1)
+
+	# Remove latitude and longitude columns
+	# if 'latitude' in list(train_df):
+	# 	train_df = train_df.drop(['latitude'], axis=1)
+	# if 'longitude' in list(train_df):
+	# 	train_df = train_df.drop(['longitude'], axis=1)
+
+	# Remove countryid column because all are same
+	if 'countryid' in list(train_df) and len(set(train_df['countryid'])) == 1:
+		train_df = train_df.drop(['countryid'], axis=1)
+
+	# Convert some columns to be categorical such as cityid, countryid, zipcpde
+	if 'cityid' in list(train_df):
+		train_df['cityid'] = train_df['cityid'].apply(lambda x: str(x)+"_categorized")
+	if 'zipcpde' in list(train_df):
+		train_df['zipcpde'] = train_df['zipcpde'].apply(lambda x: str(x)+"_categorized")
+
+	# - Other files
+	# 
+	# Get rid of area_type, area_id: These features should not be used for using LOF because they are unique
+	if 'area_id' in list(train_df) and 'area_type' in list(train_df):
+		train_df = train_df.drop(['area_id', 'area_type'], axis=1)
+
+	# Combine City and State to be one column
+	# Or remove them because they are unique (or almost unique in some files)
+	if 'City' in list(train_df) and 'State' in list(train_df):
+		# train_df["CityState"] = train_df[['City', 'State']].apply(lambda x: ''.join(x), axis=1)
+		train_df = train_df.drop(['City', 'State'], axis=1)
+
+	# - All files
+	# 
 	# Get rid of area_type: it is same for all rows
-	train_df = df.drop(['area_id', 'area_type'], axis=1)
 	train_df = train_df.loc[:, ~train_df.columns.str.contains('^Unnamed')]	# Remove Unnamed columns if there is
 
-	# Categories to numeric
+	# Do one hot to convert categorical data to numeric data
 	train_df = pd.get_dummies(train_df)
 
 	print("========== Sample train data ==========")
@@ -37,18 +82,13 @@ def LOF(data_file_path):
 	print(train_data[:10])
 	print()
 
+	# ===== Local Outlier Factors ===== #
+	# 
 	# Try different k
-	for k in [5, 10, 20]:
+	for k in k_list:
 		# Fit the model
 		clf = LocalOutlierFactor(n_neighbors=k)
 		y_pred = clf.fit_predict(train_data)
-
-		# Check that dataframe and array give the same result
-		y_df_pred = clf.fit_predict(pd.DataFrame(train_data))
-		for i, y in enumerate(y_pred):
-			if y != y_df_pred[i]:
-				print("Not same at", i)
-				sys.exit()
 
 		# Count number of outliers
 		outlier_number = 0
@@ -61,23 +101,40 @@ def LOF(data_file_path):
 		print("Total:", len(y_pred))
 		print("Number of outliers:", outlier_number)
 		print("Number of non-outliers:", (len(y_pred)-outlier_number))
+		print("Percentage of outliers:", "{0:.2f}%".format(outlier_number/(len(y_pred)-outlier_number)*100))
 		print("Oulier result:", y_pred)
 		print()
 
 
 def main():
+	"""
+	Run Local Outlier Factor algorithm to find outliers
+	Uncomment to activate that file path since we have many cleaned files
+	"""
+
 	# Just set width of output when printing dataframe (Fit my screen)
 	pd.set_option('display.width', 170)
 
 	# Where this script is
-	global SCRIPT_DIR
 	SCRIPT_DIR = os.path.abspath(os.path.dirname(sys.argv[0]))
 
-	# Set data file
-	data_file_path = SCRIPT_DIR + "/../../input/clean/crime_counts_CLEANED.csv"
-	LOF(data_file_path)
-
+	# Select data file to apply LOF or loop for do many files
+	data_file_paths = [None]*6
+	# data_file_paths[0] = SCRIPT_DIR + "/../../input/clean/Zillow_Cleaned.csv"				# 11.11
+	# data_file_paths[1] = SCRIPT_DIR + "/../../input/clean/crime_counts_CLEANED.csv"		# 11.16%
+	# data_file_paths[2] = SCRIPT_DIR + "/../../input/clean/crime_rates_CLEANED.csv"		# 11.11%
+	# data_file_paths[3] = SCRIPT_DIR + "/../../input/clean/earning_info_CLEANED.csv"		# 11.14%
+	# data_file_paths[4] = SCRIPT_DIR + "/../../input/clean/gdp_info_CLEANED.csv"			# 11.11%
+	data_file_paths[5] = SCRIPT_DIR + "/../../input/clean/graduation_rates_CLEANED.csv"	# 11.42%
 	
+	# Local Outlier Factor
+	for i in range(len(data_file_paths)):
+		if data_file_paths[i] != None:
+			print("===============================================")
+			print("========== Data file path number %i: ==========" % i)
+			print("===============================================\n")
+			LOF(data_file_paths[i])
+			print()
 
 
 if __name__ == "__main__":
